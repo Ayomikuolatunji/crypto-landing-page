eaadcf5606d3d3d609d48bbffd750e4e770b16f9
Ayomikuolatunji <ayomikuolatunji@gmail.com>
2021-10-07 18:21:42 +0100

    fourth commit

--- src/components/lunch/Lunch.js
+++ src/components/lunch/Lunch.js
@@ -6,9 +6,6 @@ export default function Lunch() {
     return (
              <main className="lunch my-12">
               <div className="lunch-wrapper flex-col">
-                <div className="lunch-img">
-                  <img src={AllData.lunch.LunchImg} alt="AllData.Mint.Mintext.title" />
-                </div>
                 <div className="lunch-content mt-10 text-center px-3">
                     <h1>{AllData.lunch.Lunchtext.title.toUpperCase()}</h1>
                     <p>{AllData.lunch.Lunchtext.text}</p>
